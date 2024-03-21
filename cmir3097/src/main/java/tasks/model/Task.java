5db93c2a8ee2fc6daf37b0e534e57b79c436de60
CristinaCri <comaniciu.crim@gmail.com>
2024-03-21 10:09:16 +0200

    changes before sonarLint

--- cmir3097/src/main/java/tasks/model/Task.java
+++ cmir3097/src/main/java/tasks/model/Task.java
@@ -21,16 +21,8 @@ public class Task implements Serializable, Cloneable {
         this.description = description;
     }
 
-    public Date getStart() {
-        return start;
-    }
-
     public void setStart(Date start) {
         this.start = start;
-    }
-
-    public Date getEnd() {
-        return end;
     }
 
     public void setEnd(Date end) {
